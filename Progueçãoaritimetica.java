c65814916866fe1bae055247f798bb3a791a2ad3
DanBrowly <61484204+DanBrowly@users.noreply.github.com>
2020-04-05 14:27:53 -0300

    eu não entedi o como era para fazer direito, eu sei que tá errado

---
+++ Progueçãoaritimetica.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    public static void main(String args[]) {
+    
+    int = PA = new int []{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+    
+     int numero = 0;
+     
+     numero + PA[];
+     
+     for(int i=0; i < numero.length i++){
+         numero + PA[];
+     }
+     
+     System.out.println("soma da PA é " + numeros);
+    
+    }
+}
