fe40d78503d2992303e063a9d6c024a689f6a2db
DanBrowly <61484204+DanBrowly@users.noreply.github.com>
2020-06-05 17:28:14 -0300

    terte

--- matrizs1ples.java
+++ matrizs1ples.java
@@ -1,12 +1,23 @@
 public class MyClass {
     public static void main(String args[]) {
       
-      double [][] matriz = new double[12][12];
+      double [][] numeral = new double[12][12];
       
       numeral [5] [0] = 5;
-      numeral [5] [1] = 5;
+      numeral [S] [1] = 5;
       numeral [5] [2] = 1.0;
       numeral [5] [3] = -3.5;
       numeral [5] [4] = 2.5;
       numeral [5] [5] = 4.1;
       
+      for ( int i=0; i<numeral.length; i++){
+          for(int j=0; j<numeral[i].length; j++){
+          
+          System.out.println(numeral[i][j]+ " ");
+          }
+      }
+    System.out.println();
+        
+    }
+    
+}
