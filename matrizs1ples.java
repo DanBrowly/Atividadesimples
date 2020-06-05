b97ca37014a3825db08c1860bf279d536dcc7b31
DanBrowly <61484204+DanBrowly@users.noreply.github.com>
2020-06-05 01:15:46 -0300

    Create matrizs1ples.java

---
+++ matrizs1ples.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+      
+      double [][] matriz = new double[12][12];
+      
+      numeral [5] [0] = 5;
+      numeral [5] [1] = 5;
+      numeral [5] [2] = 1.0;
+      numeral [5] [3] = -3.5;
+      numeral [5] [4] = 2.5;
+      numeral [5] [5] = 4.1;
+      
